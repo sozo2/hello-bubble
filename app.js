8a1572db35decb062789f80eea2f722562e62047
Samantha Zoeller <sam.zoeller@gmail.com>
2019-09-01 12:53:04 -0600

    adding proper credentials for s3

--- app.js
+++ app.js
@@ -54,8 +54,8 @@ var aws = require('aws-sdk');
 
 var s3 = new aws.S3({
     region: 'us-east-2',
-    // secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-    // accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
     bucket: 'hello-bubble'
 });
 
